592d37fcbc1f56b41debd4d536690d3de2b7b629
Ferrari Joris <dedjoris09@gmail.com>
2022-07-31 18:24:24 +0200

    color and localstorage

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -6,8 +6,12 @@
     const productId = getProductId()
     const product = await getProduct(productId)
     hydrateProduct(product)
-    addpanier(product)
+   
+    const data = addPanier(product)
+    const cont = equalverif(data)
+    colorsValue(product)
     
+    //resultQuantity(cont)
 })()
 
 
@@ -31,13 +35,70 @@ function hydrateProduct(product){
 }
 
 
-function addpanier(product){
-const addPanier = document.getElementById('addToCart');
-addPanier.addEventListener('click',function(){
-        localStorage.setItem(`id`,`${product._id}`),
-        localStorage.setItem('quantity', '1')
-    })
+//permet de verifier les données du localStorage et du panier et de créer si localStorage = null 
+function equalverif(data){
+    let equalObject = JSON.parse((localStorage.getItem('userData')))
+    const add = document.getElementById('addToCart');
+    add.addEventListener('click', function(){
+    if( equalObject === null){
+        localStorage.setItem('userData', dataJSON); 
+    }
+    else{let obj = equalObject.id
+        let objId = JSON.parse(dataJSON)
+        let obj2 = objId.id
+        let result = obj === obj2
+        return result
+    }})
 }
+
+/**function resultQuantity(cont){
+    const add = document.getElementById('addToCart');
+    add.addEventListener('click', function(){
+    if(result== true){
+        console.log('créer quantity')
+    }
+    else{
+        console.log('error')
+    }})
+}*/
+
+// créer un variable data qui contien le produit 
+function addPanier(product){
+    let output = ""
+    let selectElement = document.querySelector("#colors")
+    selectElement.addEventListener("change",function(){
+        let select = selectElement.selectedIndex - 1;
+        return output = product.colors[select]
+    })
+    selectElement.addEventListener("change",function(){
+    let data = {
+        id: `${product._id}`,
+        quantity: 1,
+        color : output
+        }
+        dataJSON = JSON.stringify(data);
+        console.log(data)
+    }) 
+}
+
+//ajoute l'option qui permet de choisir les couleurs 
+function colorsValue(product){
+    let colors = product.colors
+    for(let i in colors){
+        document.querySelector("#colors").innerHTML += `<option value="${colors[i]}">${colors[i]}</option>`;
+    }  
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
